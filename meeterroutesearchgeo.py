4ef9b9ed0feacd909d0a3226e2adcddc6c117c66
maximdea <88614175+maximdea@users.noreply.github.com>
2021-08-08 13:01:19 +0300

    Update meeterroutesearchgeo.py

--- meeterroutesearchgeo.py
+++ meeterroutesearchgeo.py
@@ -5,4 +5,4 @@ node["railway"="station"]["station"="subway"]
  (55.078,36.573,56.225,39.095);
 out;""")
 for station in r.nodes:
-	print(station.lat, station.lon, end="\n")
+	print(station.name, end="\n")
